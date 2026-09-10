c6da52687f9af31c550006e5cf8305478c522a0a
Lotta <lotsulotsu@gmail.com>
2026-09-10 13:20:07 +0300

    added styling to main section

--- cv-application/src/components/Form/CVForm.jsx
+++ cv-application/src/components/Form/CVForm.jsx
@@ -1,42 +1,44 @@
 function CVForm({ personalInfo, setPersonalInfo }) {
   return (
     <>
-      <h2>Form renders here</h2>
-      <form action="">
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={personalInfo.name}
-          onChange={(event) =>
-            setPersonalInfo({ ...personalInfo, name: event.target.value })
-          }
-        />
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={personalInfo.email}
-          onChange={(event) =>
-            setPersonalInfo({ ...personalInfo, email: event.target.value })
-          }
-        />
-        <label htmlFor="phoneNumber">Phone Number:</label>
-        <input
-          type="tel"
-          name="tel"
-          id="tel"
-          value={personalInfo.phoneNumber}
-          onChange={(event) =>
-            setPersonalInfo({
-              ...personalInfo,
-              phoneNumber: event.target.value,
-            })
-          }
-        />
-      </form>
+      <div id="CVFormSection">
+        <h2>Form renders here</h2>
+        <form action="">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={personalInfo.name}
+            onChange={(event) =>
+              setPersonalInfo({ ...personalInfo, name: event.target.value })
+            }
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={personalInfo.email}
+            onChange={(event) =>
+              setPersonalInfo({ ...personalInfo, email: event.target.value })
+            }
+          />
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input
+            type="tel"
+            name="tel"
+            id="tel"
+            value={personalInfo.phoneNumber}
+            onChange={(event) =>
+              setPersonalInfo({
+                ...personalInfo,
+                phoneNumber: event.target.value,
+              })
+            }
+          />
+        </form>
+      </div>
     </>
   );
 }
