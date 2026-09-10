9ef8ab665058497e6319918a117eb0baf5829782
Lotta <lotsulotsu@gmail.com>
2026-09-10 13:15:07 +0300

    how the preview renders information

--- cv-application/src/components/Preview/CVPreview.jsx
+++ cv-application/src/components/Preview/CVPreview.jsx
@@ -1,5 +1,22 @@
-function CVPreview() {
-  return <h2>Preview renders here</h2>;
+function CVPreview({ personalInfo }) {
+  return (
+    <>
+      <div id="CVPreviewSection">
+        <h2>Preview of the CV</h2>
+        <div id="personalInformation">
+          <h3>Personal Information</h3>
+          <dl>
+            <dt>Name: </dt>
+            <dd>{personalInfo.name}</dd>
+            <dt>Email:</dt>
+            <dd>{personalInfo.email}</dd>
+            <dt>Phone Number:</dt>
+            <dd>{personalInfo.phoneNumber}</dd>
+          </dl>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CVPreview;
