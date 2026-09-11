7a3f0cb0742c60befd1155db80597bc9b9162d93
Lotta <lotsulotsu@gmail.com>
2026-09-11 17:59:32 +0300

    moved the personal information section to own file

--- cv-application/src/components/Form/CVForm.jsx
+++ cv-application/src/components/Form/CVForm.jsx
@@ -1,3 +1,5 @@
+import PersonalInfoForm from "./PersonalInfoForm";
+
 function CVForm({ personalInfo, setPersonalInfo }) {
   return (
     <>
@@ -5,45 +7,11 @@ function CVForm({ personalInfo, setPersonalInfo }) {
         <h2>Form renders here</h2>
         <form action="">
           <legend>Personal Information</legend>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={personalInfo.name}
-              onChange={(event) =>
-                setPersonalInfo({ ...personalInfo, name: event.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={personalInfo.email}
-              onChange={(event) =>
-                setPersonalInfo({ ...personalInfo, email: event.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="phoneNumber">Phone Number:</label>
-            <input
-              type="tel"
-              name="tel"
-              id="tel"
-              value={personalInfo.phoneNumber}
-              onChange={(event) =>
-                setPersonalInfo({
-                  ...personalInfo,
-                  phoneNumber: event.target.value,
-                })
-              }
-            />
-          </div>
+
+          <PersonalInfoForm
+            personalInfo={personalInfo}
+            setPersonalInfo={setPersonalInfo}
+          />
           <legend>Education</legend>
           <div>
             <label htmlFor="schoolName">School:</label>
---
+++ cv-application/src/components/Form/PersonalInfoForm.jsx
@@ -0,0 +1,47 @@
+function PersonalInfoForm({ personalInfo, setPersonalInfo }) {
+  return (
+    <>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={personalInfo.name}
+          onChange={(event) =>
+            setPersonalInfo({ ...personalInfo, name: event.target.value })
+          }
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={personalInfo.email}
+          onChange={(event) =>
+            setPersonalInfo({ ...personalInfo, email: event.target.value })
+          }
+        />
+      </div>
+      <div>
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input
+          type="tel"
+          name="tel"
+          id="tel"
+          value={personalInfo.phoneNumber}
+          onChange={(event) =>
+            setPersonalInfo({
+              ...personalInfo,
+              phoneNumber: event.target.value,
+            })
+          }
+        />
+      </div>
+    </>
+  );
+}
+
+export default PersonalInfoForm;
