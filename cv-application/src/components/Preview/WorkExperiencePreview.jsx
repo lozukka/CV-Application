79fb195b4640255e1e03ce92ff00e77cbd84bc2b
Lotta <lotsulotsu@gmail.com>
2026-09-16 18:18:03 +0300

    styling work section

--- cv-application/src/components/Preview/WorkExperiencePreview.jsx
+++ cv-application/src/components/Preview/WorkExperiencePreview.jsx
@@ -4,16 +4,13 @@ function WorkExperiencePreview({ workExperience }) {
       <div id="workInformation">
         <h3>Work Experience</h3>
         {workExperience.map((work, index) => (
-          <dl key={index}>
-            <dt>Company:</dt>
-            <dd>{work.workPlace}</dd>
-            <dt>Title:</dt>
-            <dd>{work.workTitle}</dd>
-            <dt>Years:</dt>
-            <dd>{work.workYears}</dd>
-            <dt>Description:</dt>
-            <dd>{work.workDescription}</dd>
-          </dl>
+          <div key={index}>
+            <h4>
+              {work.workTitle}, {work.workPlace}
+            </h4>
+            <p>{work.workYears}</p>
+            <p>{work.workDescription}</p>
+          </div>
         ))}
       </div>
     </>
